359eb359a6a958c3c1a2ba16483c3e4805431672
Mikolay Bashta <mikolay4401@ukr.net>
2020-01-21 17:42:32 +0200

    Add new fail dataTypes.js. Lesson 2

---
+++ Lesson 2/dataTypes.js
@@ -0,0 +1,43 @@
+function show(data){
+    console.log(data);
+}
+
+//var value = 0;
+//
+//if (value>0){
+//    show('The number is greater than 0');
+//}
+//
+//else if(value<0){
+//    show('The number is less than 0')
+//}
+//
+//else{
+//    ('The number is 0')
+//}
+
+
+var login = 'Admin'
+var password = 'TheMaster'
+
+if(login == 'Admin'){
+    if(password == 'TheMaster'){
+        show('Welcom');
+    }
+    
+    else if(password == 'Other'){
+        show('Wrong password')
+    }
+    
+    else{
+        show('Canceled');
+    }
+}
+
+else if(login == 'Other'){
+    show('I dont know you')
+}
+
+else{
+    show('Canceled')
+}
